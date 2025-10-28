bb0c7beea734cf8aa7e205aa1cbbb969f5184638
MOHANA PRASANTH S <98017458+Mohan-321ret@users.noreply.github.com>
2025-10-28 11:04:50 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL ||https://e-wallet-backend-e6gf.onrender.com;
+const BASE_URL = "https://e-wallet-backend-e6gf.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
